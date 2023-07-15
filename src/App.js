779b65b4f0ec01e002641b1c3cf4e4fac61ad1a0
Kadirov1993 <mkodirjonov@mail.ru>
2023-07-15 23:44:14 +0200

    first commet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box, Button, AppBar, Typography, Toolbar, Stack} from '@mui/material'
+import { Email, Settings} from '@mui/icons-material';
+import DropDown from './components/DropDown';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar sx={{display: 'flex', justifyContent: 'space-between', backgroundColor: '#4dd0e1'}}>
+           <Button variant='text' sx={{color: 'white'}}>
+            Navbar
+           </Button>
+
+           <Box sx={{display: 'flex', alignItems: 'center'}}>
+              <Button startIcon={<Email/>} sx={{color: 'white'}}>
+                Contact
+              </Button>
+              <Button startIcon={<Settings/>} sx={{color: 'white'}}>
+                Settings
+              </Button>
+              <DropDown/>
+           </Box>
+        </Toolbar>
+      </AppBar>
+
+      
+
+      <Box sx={{ flexGrow: 1 }} marginTop={20}>
+      <AppBar position="static">
+         <Toolbar  sx={{display: 'flex', justifyContent: 'space-between'}} >
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Femmecubator
+          </Typography>
+
+          <Box>
+             <Stack spacing={5} direction="row">
+                 <Button color="inherit">Listings</Button>
+                 <Button color="inherit">Mentors</Button>
+                 <Button color="inherit">My Account</Button>
+                 <Button color="inherit">Log out</Button>
+             </Stack>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </>
   );
 }
 
---
+++ src/components/DropDown.js
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import {Person, KeyboardArrowDown} from '@mui/icons-material';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+
+const options = ['My Account', 'Log out'];
+
+export default function DropDown() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <ButtonGroup variant="contained" ref={anchorRef}  sx={{boxShadow: 'none'}}  >
+        <Button startIcon={<Person/>} endIcon={<KeyboardArrowDown/>} onClick={handleToggle} sx={{
+            backgroundColor: 'inherit',
+            boxShadow: 'none',
+            border: '0',
+            '&: hover': {
+                backgroundColor: 'inherit'
+            }
+          }}>Profile</Button>
+        
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper sx={{ marginRight: '10px'}}>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      disabled={index === 2}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </React.Fragment>
+  );
+}
